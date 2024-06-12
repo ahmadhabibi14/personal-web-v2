4ee159b2190a05ed42017dcfc9c06779275c8abc
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2024-06-12 18:39:28 +0800

    [feat] add svelte, implement tailwindcss classes for layout

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Ahmad Rizky Nusantara Habibi';
+export const SITE_DESCRIPTION = 'A personal web and blog site of Ahmad Habibi';
---
+++ src/data/links.ts
@@ -0,0 +1,12 @@
+type link = {
+  path: string;
+  label: string;
+}
+
+export const links: link[] = [
+  {label: 'Home', path: '/'},
+  {label: 'About', path: '/about'},
+  {label: 'Project', path: '/project'},
+  {label: 'Contact', path: '/contact'},
+  {label: 'Blog', path: '/blog'}
+]
