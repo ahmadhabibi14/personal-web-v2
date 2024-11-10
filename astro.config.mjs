c08066f195a87b66d8c0cdc65d1b446474a69b65
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2024-11-10 12:02:11 +0800

    [refactor] use cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://habi14.pages.dev/",
   integrations: [react(), tailwind(), icon(), mdx()],
   output: "server",
+  adapter: cloudflare(),
 });
